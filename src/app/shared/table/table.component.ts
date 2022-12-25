b695393dd037d71ff5458d6a56004426425553dc
gerasalta <geragutierrezsalta@gmail.com>
2022-12-25 18:14:59 -0300

    table titles success

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class TableComponent {
 
+  public ELEMENT_DATA = [
+    {name: 'Hydrogen', lastName: 1.0079, address: 'H'},
+    {name: 'Hydrogen', lastName: 1.0079, address: 'H'},
+    {name: 'Hydrogen', lastName: 1.0079, address: 'H'}
+  ];
+  displayedColumns: string[] = ['Nombre', 'Apellido', 'Direccion'];
+  dataSource = this.ELEMENT_DATA;
+
+  constructor(){}
+
+  ngOnInit(){}
+
+
 }
